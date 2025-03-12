a3c565fcd72e017ea197ae3d468b13d4838d2e67
Hasono <2105689721@qq.com>
2025-03-12 10:37:03 +0800

    完成Button组件

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Button from "./components/Button/Button.vue";
+</script>
+
+<template>
+  <div class="button">
+    <div class="basic block">
+      <Button> hello </Button>
+      <Button type="primary"> Primary </Button>
+      <Button type="success"> Success </Button>
+      <Button type="warning"> Warning </Button>
+      <Button type="danger"> Danger </Button>
+      <Button type="info"> Info </Button>
+    </div>
+
+    <div class="plain block">
+      <Button plain> hello </Button>
+      <Button type="primary" plain> Primary </Button>
+      <Button type="success" plain> Success </Button>
+      <Button type="warning" plain> Warning </Button>
+      <Button type="danger" plain> Danger </Button>
+      <Button type="info" plain> Info </Button>
+    </div>
+
+    <div class="round block">
+      <Button round> hello </Button>
+      <Button type="primary" round> Primary </Button>
+      <Button type="success" round> Success </Button>
+      <Button type="warning" round> Warning </Button>
+      <Button type="danger" round> Danger </Button>
+      <Button type="info" round> Info </Button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.block {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from "./types";
+
+defineOptions({
+  name: "VkButton",
+});
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: "button",
+});
+
+const buttonRef = ref<HTMLButtonElement>();
+
+defineExpose({
+  ref: buttonRef,
+});
+</script>
+
+<template>
+  <button
+    ref="buttonRef"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+    :disabled="disabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
