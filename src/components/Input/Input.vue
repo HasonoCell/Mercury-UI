552727f2f862e7a78db858b83447ad435f760f1f
Hasono <2105689721@qq.com>
2025-03-17 20:37:37 +0800

    完成Input组件和测试

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,180 @@
+<script setup lang="ts">
+import { computed, ref, useAttrs, watch } from "vue";
+import type { InputEmits, InputProps } from "./types";
+import type { Ref } from "vue";
+import Icon from "../Icon/Icon.vue";
+import { nextTick } from "vue";
+
+defineOptions({
+  name: "VkInput",
+  inheritAttrs: false,
+});
+
+const props = withDefaults(defineProps<InputProps>(), {
+  type: "text",
+  autocomplete: "off",
+});
+
+const emits = defineEmits<InputEmits>();
+
+const attrs = useAttrs();
+
+const innerValue = ref<string>(props.modelValue);
+
+const isFocus = ref(false);
+
+const passwordVisible = ref(false);
+
+const inputRef = ref() as Ref<HTMLElement>;
+
+const showClear = computed(
+  () => props.clearable && !props.disabled && !!innerValue.value && isFocus.value
+);
+
+const showPasswordArea = computed(
+  () => props.showPassword && !props.disabled && !!innerValue.value
+);
+
+const handleInput = () => {
+  emits("update:modelValue", innerValue.value);
+  emits("input", innerValue.value);
+};
+
+const handleChange = () => {
+  emits("change", innerValue.value);
+};
+
+const handleFocus = (event: FocusEvent) => {
+  isFocus.value = true;
+  emits("focus", event);
+};
+
+const keepFocus = async () => {
+  await nextTick();
+  inputRef.value.focus();
+};
+
+const handleBlur = (event: FocusEvent) => {
+  isFocus.value = true;
+  emits("blur", event);
+};
+
+const clear = () => {
+  innerValue.value = "";
+  emits("update:modelValue", "");
+  emits("clear");
+  emits("input", "");
+  emits("change", "");
+};
+
+const togglePasswordVisible = () => {
+  passwordVisible.value = !passwordVisible.value;
+};
+
+// 占位函数，消除 blur 带来的影响
+const NOOP = () => {}
+
+watch(
+  () => props.modelValue,
+  (newVal) => {
+    innerValue.value = newVal;
+  }
+);
+
+defineExpose({
+  ref: inputRef,
+});
+</script>
+
+<template>
+  <div
+    class="vk-input"
+    :class="{
+      [`vk-input--${type}`]: type,
+      [`vk-input--${size}`]: size,
+      'is-disabled': disabled,
+      'is-prepend': $slots.prepend,
+      'is-append': $slots.append,
+      'is-prefix': $slots.prefix,
+      'is-suffix': $slots.suffix,
+      'is-focus': isFocus,
+    }"
+  >
+    <!-- input -->
+    <template v-if="type !== 'textarea'">
+      <!-- prepend slot -->
+      <div v-if="$slots.prepend" class="vk-input__prepend">
+        <slot name="prepend" />
+      </div>
+      <div class="vk-input__wrapper">
+        <!-- prefix slot -->
+        <span v-if="$slots.prefix" class="vk-input__prefix">
+          <slot name="prefix" />
+        </span>
+        <!-- main input -->
+        <input
+          :type="showPassword ? (passwordVisible ? 'text' : 'password') : type"
+          v-bind="attrs"
+          class="vk-input__inner"
+          :disabled="disabled"
+          :readonly="readonly"
+          :autocomplete="autocomplete"
+          :placeholder="placeholder"
+          :autofocus="autofocus"
+          :form="form"
+          v-model="innerValue"
+          @input="handleInput"
+          @focus="handleFocus"
+          @blur="handleBlur"
+          @change="handleChange"
+          ref="inputRef"
+        />
+        <!-- suffix slot -->
+        <span
+          v-if="$slots.suffix || showClear || showPasswordArea"
+          class="vk-input__suffix"
+          @click="keepFocus"
+        >
+          <slot name="suffix" />
+          <Icon icon="circle-xmark" v-if="showClear" class="vk-input__clear" @click="clear" @mousedown.prevent="NOOP" />
+          <Icon
+            icon="eye"
+            v-if="showPasswordArea && passwordVisible"
+            class="vk-input__password"
+            @click="togglePasswordVisible"
+          />
+          <Icon
+            icon="eye-slash"
+            v-if="showPasswordArea && !passwordVisible"
+            class="vk-input__password"
+            @click="togglePasswordVisible"
+          />
+        </span>
+        <!-- append slot -->
+        <div v-if="$slots.append" class="vk-input__append">
+          <slot name="append" />
+        </div>
+      </div>
+    </template>
+
+    <!-- textarea -->
+    <template v-else>
+      <textarea
+        v-bind="attrs"
+        class="vk-textarea__wrapper"
+        :disabled="disabled"
+        :readonly="readonly"
+        :autocomplete="autocomplete"
+        :placeholder="placeholder"
+        :autofocus="autofocus"
+        :form="form"
+        v-model="innerValue"
+        @input="handleInput"
+        @focus="handleFocus"
+        @blur="handleBlur"
+        @change="handleChange"
+        ref="inputRef"
+      ></textarea>
+    </template>
+  </div>
+</template>
---
+++ src/components/Input/types.ts
@@ -0,0 +1,28 @@
+export interface InputProps {
+  type?: "text" | "textarea" | "password";
+  size?: "small" | "large";
+  modelValue: string;
+  clearable?: boolean;
+  showPassword?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+  readonly?: boolean;
+  autocomplete?: string;
+  autofocus?: boolean;
+  form?: string;
+}
+
+export interface InputEmits {
+  // change 事件指的是值有变化了，并且失去了 focus
+  (e: "change", value: string): void;
+  // input 事件指的是值有了变化就算
+  (e: "input", value: string): void;
+  (e: "focus", value: FocusEvent): void;
+  (e: "blur", value: FocusEvent): void;
+  (e: "clear"): void;
+  (e: 'update:modelValue', value: string): void;
+}
+
+export interface InputInstance {
+  ref: HTMLInputElement | HTMLTextAreaElement;
+}
